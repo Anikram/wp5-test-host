054d5904d498c16f35fad2e985f275d7cf43c65d
anikram <anikram@192.168.1.41>
2023-01-04 00:21:32 +0300

    Adding a dog

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import DogWidget from './DogWidget';
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <div><DogWidget /></div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/DogWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DogWidget = ({}) => {
+  return (
+    <img src="https://placedog.net/500/280/id=5" alt="" style={{width: 500, padding: '1em', border: '2em dashed blue' }}/>
+  )
+}
+
+export default DogWidget;
